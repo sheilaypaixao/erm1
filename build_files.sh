9dc0772535af1f3aaeeb6664d21df4ac74d804f2
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 18:43:41 -0300

    teste

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@ echo "Building the project..."
 curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3.12 get-pip.py
 
-apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
+python3.9 -m venv venv
+source venv/bin/activate
 
-python3.12 -m pip install -r requirements.txt
+pip install -r requirements.txt
