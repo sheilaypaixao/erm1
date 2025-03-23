09720d875142bb2f2db94e0c07709793b5a5831d
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 16:29:57 -0300

    versao python

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,4 @@ echo "Building the project..."
 curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 
-pip install -r requirements.txt
+python3 pip install -r requirements.txt
