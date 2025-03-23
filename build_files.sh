506ef486bbe714a89c1d6680e6d2829e535240bf
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 18:36:43 -0300

    teste

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,6 @@ echo "Building the project..."
 curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3.12 get-pip.py
 
+apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
+
 python3.12 -m pip install -r requirements.txt
