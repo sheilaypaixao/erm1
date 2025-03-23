0631e23ed9a9ebc020f83e16a71189c13a386ca6
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 18:46:56 -0300

    versao

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ echo "Building the project..."
 curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3.12 get-pip.py
 
-python3.9 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
