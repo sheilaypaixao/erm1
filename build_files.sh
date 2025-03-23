fed5ad408c43ef725be67a1c8888327dda82a671
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 16:14:58 -0300

    teste

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "Building the project..."
 curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
-python3.9 get-pip.py
+python3 get-pip.py
 
-python3.9 -m ensurepip
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
