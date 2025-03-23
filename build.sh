8f6b06257abdbfd8bc236199959c65f645b7b090
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-23 15:52:49 -0300

    teste

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ curl -L -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3.9 get-pip.py
 
 apt-get install pkg-config
-
-pip install -r requirements.txt
+python3.9 -m ensurepip
+python3.9 -m pip install -r requirements.txt
