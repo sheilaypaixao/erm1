bb1e6ee80839f378ac2d9169e667a5c56de48803
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-24 20:46:32 -0300

    trocar libs

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 echo "Building the project..."
 
 yum install pkg-config
-yum install gcc
-yum install -y mysql-devel
+yum install make glibc-devel gcc patch
+yum install mariadb105-devel
 
 python3.12 -m venv venv
 source venv/bin/activate
