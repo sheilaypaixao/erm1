8fd3bbc57b2fd389575a8cb2b8a367a799b716b0
Sheila Paixão <sheila.y.pa@gmail.com>
2025-03-24 21:29:31 -0300

    teste

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 echo "Building the project..."
 
-yum install pkg-config
-yum install make glibc-devel gcc patch
 yum install mariadb105-devel
+mysql_config --cflags
+mysql_config --libs
 
 python3.12 -m venv venv
 source venv/bin/activate
