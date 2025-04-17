ac5ffbcdb290dba38d5a29c9a2c2de292e3a213b
Sheila Paixão <sheila.y.pa@gmail.com>
2025-04-17 19:41:40 -0300

    url

--- erm/settings.py
+++ erm/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['.vercel.app', 'localhost:5173', 'localhost:8000', 'localhost']
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://ermfrontend.vercel.app/",
+    "http://localhost:5173/"
 ]
 
 # Application definition
